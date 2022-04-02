53951b71a995714006732a23371415e8da778b03
networktraffic <88440862+networktraffic@users.noreply.github.com>
2022-04-02 03:37:08 -0400

    Update src.lua

--- src.lua
+++ src.lua
@@ -2,35 +2,35 @@ local timer = { } do
     timer.__index = timer
     
     function timer.new( )
-        return setmetatable( { currTime = 0, finishedTime = 0 }, timer )
+        return setmetatable( { curr_time = 0, finished_time = 0 }, timer )
     end
     
     function timer:start( )
-        if self.currTime ~= 0 then
+        if self.curr_time ~= 0 then
             return
         end
         
-        self.currTime = os.clock( )
+        self.curr_time = os.clock( )
     end
     
     function timer:stop( )
-        if self.currTime ~= 0 then 
-            self.finishedTime = os.clock( ) - self.currTime
-            self.currTime = 0
+        if self.curr_time ~= 0 then 
+            self.finished_time = os.clock( ) - self.curr_time
+            self.curr_time = 0
         end
     end
     
     function timer:restart( )
-        if self.finishedTime ~= 0 then
-            self.finishedTime = 0
+        if self.finished_time ~= 0 then
+            self.finished_time = 0
             self:start( )
         end
     end
 	
     function timer:reset( )
-	 if self.currTime ~= 0 or self.finishedTime ~= 0 then
-	     self.currTime = 0
-             self.finishedTime = 0
+	 if self.curr_time ~= 0 or self.finished_time ~= 0 then
+	     self.curr_time = 0
+             self.finished_time = 0
 	 end
     end
 	
@@ -41,7 +41,7 @@ local timer = { } do
 	 func( )
         
 	 self:stop( )
-	 return self.finishedTime
+	 return self.finished_time
      end
 end
 
