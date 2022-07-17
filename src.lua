a2b8a2c6d9660aa604169f31f772642a372cb665
network <88440862+networktraffic@users.noreply.github.com>
2022-07-17 10:03:56 -0400

    Update src.lua

--- src.lua
+++ src.lua
@@ -1,48 +1,47 @@
-local timer = { } do
-    timer.__index = timer
-    
-    function timer.new( )
-        return setmetatable( { curr_time = 0, finished_time = 0 }, timer )
+local timer = { }
+timer.__index = timer
+
+function timer.new( )
+    return setmetatable( { curr_time = 0, finished_time = 0 }, timer )
+end
+
+function timer:start( )
+    if self.curr_time ~= 0 then
+        return
     end
-    
-    function timer:start( )
-        if self.curr_time ~= 0 then
-            return
-        end
-        
-        self.curr_time = os.clock( )
+
+    self.curr_time = os.clock( )
+end
+
+function timer:stop( )
+    if self.curr_time ~= 0 then 
+        self.finished_time = os.clock( ) - self.curr_time
+        self.curr_time = 0
     end
-    
-    function timer:stop( )
-        if self.curr_time ~= 0 then 
-            self.finished_time = os.clock( ) - self.curr_time
-            self.curr_time = 0
-        end
+end
+
+function timer:restart( )
+    if self.finished_time ~= 0 then
+        self.finished_time = 0
+        self:start( )
     end
-    
-    function timer:restart( )
-        if self.finished_time ~= 0 then
-            self.finished_time = 0
-            self:start( )
-        end
-    end
+end
+
+function timer:reset( )
+    if self.curr_time ~= 0 or self.finished_time ~= 0 then
+        self.curr_time = 0
+        self.finished_time = 0
+	end
+end
 	
-    function timer:reset( )
-	 if self.curr_time ~= 0 or self.finished_time ~= 0 then
-	     self.curr_time = 0
-             self.finished_time = 0
-	 end
-    end
-	
-    function timer:wrap( func )
-    	 self:reset( )
-	 self:start( )
-        
-	 func( )
-        
-	 self:stop( )
-	 return self.finished_time
-     end
+function timer:measure_speed( func )
+    self:reset( )
+	self:start( )
+
+	func( )
+  
+	self:stop( )
+	return self.finished_time
 end
 
 return timer
